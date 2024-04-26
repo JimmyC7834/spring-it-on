2b1d3bfd686651acf8a88c3acf04d17e2d1846b4
jc4649 <jc4649@cs.washington.edu>
2024-04-26 03:15:51 -0700

    added texture and stcns

---
+++ player.gd
@@ -0,0 +1,90 @@
+extends CharacterBody2D
+
+@onready var sprite = $Sprite2D
+
+@export var terminal_v_velocity: float = 50.0
+@export var gravity: float = 50.0
+
+@export var damping: float = 0.25
+@export var bounce_threshold: float = 5.0
+@export var max_jump_force: float = 30.0
+@export var h_jump_force: float = 5.0
+@export var jump_force_acc: float = 30.0
+@export var speed: float = 500.0
+var dir: int = 0
+var jump_force: float = 0.0
+var bouncing: bool = false
+var state: Callable = state_falling
+
+signal on_bounced
+signal on_landed
+
+func _physics_process(delta):
+    apply_gravity(delta)
+    state.call(delta)
+    move_and_slide()
+
+func state_floor(delta):
+    if Input.is_action_just_pressed("DOWN"):
+        velocity.x = 0    
+        state = state_jump
+        return
+    
+    if Input.is_action_just_pressed("UP"):
+        dir = 0
+
+    var axis = Input.get_axis("LEFT", "RIGHT")
+    if axis != 0:
+        dir = axis
+    velocity.x = speed * axis
+
+func state_jump(delta):
+    jump_force += jump_force_acc * delta
+    jump_force = min(max_jump_force, jump_force)
+    if Input.is_action_just_released("DOWN"):
+        jump()
+        state = state_falling
+
+func state_falling(delta):
+    if Input.is_action_pressed("UP"):
+        state = state_bouncing
+        return
+
+    move_and_collide(velocity)
+    sprite.rotation = velocity.angle()
+    if is_on_floor():
+        sprite.rotation = 0
+        on_landed.emit()    
+        
+        state = state_floor
+
+func state_bouncing(delta):
+    if Input.is_action_just_released("UP"):
+        state = state_falling
+        return
+    
+    var collision = move_and_collide(velocity)       
+    sprite.rotation = velocity.angle()
+    
+    if collision:
+        if abs(velocity.y) < bounce_threshold:
+            sprite.rotation = 0
+            state = state_floor
+            
+        bounce(collision)
+        on_bounced.emit()
+
+func apply_gravity(delta: float):
+    velocity.y += gravity * delta
+    velocity.y = min(terminal_v_velocity, velocity.y)
+
+func jump():
+    velocity = Vector2(dir * h_jump_force, -jump_force)
+    jump_force = 0
+
+func bounce(collision: KinematicCollision2D):
+    if not collision: return
+    velocity = velocity.bounce(collision.get_normal()) * (1 - damping)
+    print("bounced: ", velocity)
+    if abs(velocity.y) < bounce_threshold:
+        velocity.y = 0
